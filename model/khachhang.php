6acdc027efe1c90c57df1d444c988f4c704ee46f
Vi-DH23th3 <tvi22869@gmail.com>
2025-11-18 10:58:56 +0700

    thêm: giỏ hàng, đặt hàng

---
+++ model/khachhang.php
@@ -0,0 +1,69 @@
+<?php 
+    class KHACHHANG{
+        public function topkhachhang(){
+            $db = DATABASE::connect();
+            try{
+                $sql = "SELECT
+                            nguoidung.hoten AS TenKhachHang,
+                            nguoidung.email AS EmailKhachHang,
+                            SUM(donhang.tongtien) AS TongTienDaMua,
+                            COUNT(donhang.id) AS SoLuongDonHang
+                        FROM
+                            nguoidung
+                        JOIN
+                            donhang ON nguoidung.id = donhang.nguoidung_id
+                        GROUP BY
+                            nguoidung.id, nguoidung.hoten, nguoidung.email
+                        ORDER BY
+                            TongTienDaMua DESC -- Sắp xếp để xem khách hàng có doanh thu cao nhất
+                        LIMIT 5";
+                $cmd = $db->prepare($sql);
+                $cmd->execute();
+                $ketqua = $cmd->fetchAll(PDO::FETCH_ASSOC);
+                return $ketqua;
+            }
+            catch(PDOException $e){
+                $error_message=$e->getMessage();
+                echo "<p>Lỗi truy vấn: $error_message</p>";
+                exit();
+            }
+        }
+            // lấy thông tin người dùng có $email
+	public function laythongtinkhachhang($email){
+		$db = DATABASE::connect();
+		try{
+			$sql = "SELECT * FROM nguoidung WHERE email=:email AND loai=3";
+			$cmd = $db->prepare($sql);
+			$cmd->bindValue(":email", $email);
+			$cmd->execute();
+			$ketqua = $cmd->fetch();
+			$cmd->closeCursor();
+			return $ketqua;
+		}
+		catch(PDOException $e){
+			$error_message=$e->getMessage();
+			echo "<p>Lỗi truy vấn: $error_message</p>";
+			exit();
+		}
+    } 
+    public function themkhachhang($email,$sodt,$hoten){
+		$db = DATABASE::connect();
+		try{
+			$sql = "INSERT INTO nguoidung(email,matkhau,sodienthoai,hoten,loai,trangthai) VALUES(:email,:matkhau,:sodt,:hoten,3,1)";
+			$cmd = $db->prepare($sql);
+			$cmd->bindValue(':email',$email);
+			$cmd->bindValue(':matkhau',md5($sodt));
+			$cmd->bindValue(':sodt',$sodt);
+			$cmd->bindValue(':hoten',$hoten);			
+			$cmd->execute();
+			$id = $db->lastInsertId();
+			return $id;
+		}
+		catch(PDOException $e){
+			$error_message=$e->getMessage();
+			echo "<p>Lỗi truy vấn: $error_message</p>";
+			exit();
+		}
+	}
+}
+?>
